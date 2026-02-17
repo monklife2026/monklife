a6358289d5c10363e35d25fffe7ac12cbc564d75
xingshuang0809 <xingshuang0809@gmail.com>
2026-02-16 22:12:01 -0500

    sitemap fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
@@ -18,8 +20,5 @@ export default defineConfig({
     }
   },
   site: 'https://monklife.pages.dev',
-  integrations: [sitemap()],
-
-
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
