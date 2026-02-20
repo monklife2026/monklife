5d53b5cdbfb6fdee07f444921efdee19be0c85ca
xingshuang0809 <xingshuang0809@gmail.com>
2026-02-20 18:38:16 -0500

    title changed2

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import heroImage from './assets/hero-real.JPG';
 export const SITE = {
   website: 'https://monklife.pages.dev', // Replace with your actual deployed URL
   author: 'MonkLife',
-  description: 'GEBIS Monks\' Real Life: How We Work with the PEI Community to Protect This Land.',
+  description: 'Explore Monks\' Real Life.',
   title: 'Monks\' Life',
   ogImage: 'astropaper-og.jpg',
   lightAndDarkMode: true,
@@ -12,15 +12,15 @@ export const SITE = {
 
   // Lab Info
   labName: 'Monks\' Life',
-  university: 'Canada',
+  university: '',
   logo: '', // Logo path
   avatar: '', // Avatar for SEO/Schema
   email: 'contact@lab.edu', // Contact email for Join Us page
 
   // Hero Section (Home Page) - Main content does not need to be translated for 8 languages by default
   hero: {
-    title: 'GEBIS Monks\' Real Life ',
-    subtitle: 'How We Work with the PEI Community to Protect This Land.',
+    title: 'Explore Monks\' Real Life.',
+    subtitle: 'A simple life, A clear mind',
     //action: 'View Our Stories', // Optional call to action text
     image: heroImage, // Hero image path
   },
